1599f37332fa756297fb6f841e61debe78342012
Wlancer <atroshonok@tut.by>
2016-08-18 16:03:19 +0300

    Added AddressDao, AdminDao, EntityDao classes

---
+++ BeansXMLMapping/src/main/java/com/atr/hibtasks/db/EntityDao.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.atr.hibtasks.db;
+
+import com.atr.hibtasks.beans.Entity;
+
+/**
+ * @author Atroshonok Ivan
+ *
+ */
+public class EntityDao extends BaseDao<Entity> {
+
+}
